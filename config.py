7196827e2735b123c61956c478515dc1a8fdb2fc
Bhavana-Doddi <bhavanadoddi5@gmail.com>
2025-07-20 13:44:39 +0530

    Updated config

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app=Flask(__name__)
-CORS(app, origins=["https://contactmanager-frontend.onrender.com"])
+CORS(app, origins=["https://contactmanager-frontend.onrender.com"], supports_credentials=True, methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"])
+
 
 
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///mydatabase.db"
